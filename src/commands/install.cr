0d91d5c541d4d27e86943e1030debaa1d4079c90
Siegfried Ehret <siegfried@ehret.me>
2020-04-28 18:05:01 +0200

    :memo: Add some comments

--- src/commands/install.cr
+++ src/commands/install.cr
@@ -9,6 +9,8 @@ module Brrr
 
         # TODO Make this parallel for multiple packages
         args.each do |package_name|
+          # Let's get this package
+
           local_path = Path[package_name].expand(Dir.current)
 
           yaml = if registry.is_local_package local_path
@@ -18,6 +20,8 @@ module Brrr
                  end
 
           File.write("#{cache.path}/#{package_name}.yaml", yaml)
+
+          # Now, time to read the package and get some info
           package = Package.from_yaml(yaml)
 
           name = package.name
@@ -35,23 +39,26 @@ module Brrr
             return
           end
 
+          # Time to download the binary
           binary = latest_binary[config.arch]
-
           start_index = (binary.url.rindex("/") || 0) + 1
           binary_name = binary.url[start_index..-1]
-
           cache_target_path = cache.path / binary_name
           Downloader.get_file(binary.url, cache_target_path)
 
+          # If a hash is provided, we verify the package
           if binary.hash && !Downloader.verify(cache_target_path, binary)
             puts "Failed to verify checksum for file #{cache_target_path}."
             return
           end
 
+          # Then we can extract from the cache to our packages folder
           Downloader.extract(cache_target_path, binary, config.packages_path / name)
 
+          # And write some symbolic links
           config.link(name, binary.binaries)
 
+          # Finally, we add the package to our installed packages
           config.add_installed_package(package_name, latest_version)
         end
       end
